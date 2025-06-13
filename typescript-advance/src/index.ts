e45135f0634aaec6404be4c56a23a0104671f11c
ayushman1210 <ayushmangupta892@gmail.com>
2025-06-13 18:40:17 +0530

    typescript learning !!

--- typescript-advance/src/index.ts
+++ typescript-advance/src/index.ts
@@ -29,10 +29,59 @@ type User2={
 age:number,
 country:string
 }
-const obj={
+const obj:User2={
  name:"atush",
     age:25,
     country:"USA"
 }
 
 // on readonly method we can not assign any new value to the readonly variable
+
+// record 
+
+type user3={
+  [key:string]:number;
+}
+type user4=Record<string,{age:number}>
+let obj2:user4={
+"ayush":{age:32}
+}
+
+const userMap = new Map<string, number>();
+
+// Set values
+userMap.set("ayush", 25);
+userMap.set("john", 30);
+
+// Get a value
+console.log(userMap.get("ayush")); // Output: 25
+
+// Delete a value
+userMap.delete("john");
+
+// Check if a key exists
+console.log(userMap.has("john")); //
+
+
+//exclude 
+type Letters = "a" | "b" | "c";
+type ExcludeB = Exclude<Letters, "b">; // "a" | "c"
+
+let letter: ExcludeB = "a"; // OK
+// letter = "b"; // ❌ Error: Type '"b"' is not assignable to type '"a" | "c"'
+ 
+// zod 
+import { z } from "zod";
+
+const userSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+});
+
+// TypeScript type inferred from the schema
+type User = z.infer<typeof userSchema>;
+
+const user: User = {
+  name: "Ayushman",
+  age: 22,
+};
